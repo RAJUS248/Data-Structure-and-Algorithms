2aa3c6fed23a5b7f80a7a772ed174b0464c7a241
Raju S Baradur <125146841+RAJUS248@users.noreply.github.com>
2025-10-13 19:53:20 +0530

    Add files via upload

---
+++ 05_Stack/creating stack with count.py
@@ -0,0 +1,69 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+class Stack:
+    def __init__(self):
+        self.top = None
+        self.count = 0
+
+    def push(self,data):
+        new_node = Node(data)
+
+        new_node.next = self.top
+        self.top = new_node
+
+        self.count += 1
+        print(f"the value {data} is pushed in stack")
+        
+
+    def pop(self):
+        if self.top is None:
+            print("stack is empty")
+            return
+        
+        popped_value = self.top.data
+        self.top = self.top.next
+        self.count -= 1
+        print("popped value is :", popped_value)
+        return popped_value
+    
+    def peek(self):
+        if self.top is None:
+            return None
+        print("peek is: ",self.top.data)
+        return self.top.data
+    
+    def get_count(self):
+        print("the count:",self.count)
+        return self.count
+    
+    def display(self):
+
+        if self.top is None:
+            print("stack is empty")
+            return
+        
+        current_top = self.top
+        while current_top:
+            print(current_top.data, end = " -> ")
+            current_top = current_top.next
+
+        print("None")
+
+if __name__ == "__main__":
+    stack = Stack()
+    stack.display()
+    stack.push(1)
+    stack.display()
+    stack.push(2)
+    stack.push(3)
+    stack.push(4)
+    stack.display()
+    stack.get_count()
+    stack.pop()
+
+    stack.display()
+    stack.peek()
+    stack.get_count()
---
+++ 05_Stack/creating stack.py
@@ -0,0 +1,64 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+class Stack:
+
+    def __init__(self):
+        self.top = None
+        
+
+    def push(self,data):
+        new_node = Node(data)
+
+        new_node.next = self.top
+        self.top = new_node
+
+    def pop(self):
+        if self.top is None:
+            print("stack is empty")
+            return
+        
+        popped_value = self.top.data        # put top value data in popped value
+        self.top = self.top.next            # top value become below the top means    3 below 2 is top now
+        print("poping value: ",popped_value)                 
+        return popped_value                 
+        
+    def peek(self):
+        if self.top is None:
+            return None
+        print("peek value :",self.top.data)
+        return self.top.data
+
+    def is_empty(self):
+        return self.top is None
+    
+    def display(self):
+        current_node = self.top
+
+        while current_node:
+            print(current_node.data, end = " -> ")
+            current_node = current_node.next
+
+        print("None")
+
+stack = Stack()
+
+stack.is_empty()
+stack.push(1)
+stack.push(2)
+stack.push(3)
+
+stack.display()
+stack.pop()
+stack.peek()
+stack.display()
+
+         
+            
+        
+       
+
+        
+    
