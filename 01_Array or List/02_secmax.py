fcb2f1e5a18d17a436c641f510932fd46b06a09f
Raju S Baradur <125146841+RAJUS248@users.noreply.github.com>
2025-11-10 21:00:10 +0530

    Update 02_secmax.py

--- 01_Array or List/02_secmax.py
+++ 01_Array or List/02_secmax.py
@@ -4,7 +4,7 @@ def sec_max(arr):
     for num in arr:
         if num > maxi:
             sec_max = maxi
-            maxi = num
+            maxi = num 
 
         elif num > sec_max and num != maxi:
             sec_max = num
@@ -18,3 +18,4 @@ def sec_max(arr):
 arr = [10, 20, 4, 45, 99]
 
 print(sec_max(arr))
+
