4d6090352bc06f825761e07439e2a615ba53e169
Raju S Baradur <125146841+RAJUS248@users.noreply.github.com>
2025-11-11 08:12:04 +0530

    Update 30_Find Transition Point.py

--- 01_Array or List/30_Find Transition Point.py
+++ 01_Array or List/30_Find Transition Point.py
@@ -16,3 +16,34 @@ def Find_Transition_Point(arr):
 
 arr = [1, 1, 1]
 print(Find_Transition_Point(arr))
+
+
+
+def Find_Transition_Point_v2(arr):
+
+    low = 0
+    high = len(arr)-1
+
+    t_point = -1
+
+    while low <= high:
+
+        mid = (low + high)//2
+
+        if arr[mid] == 1:
+            
+            t_point = mid
+
+            high = mid - 1
+
+        else:
+
+            low = mid + 1
+
+    return t_point
+
+arr = [0,0,0,1,1, 1, 1]
+print(Find_Transition_Point_v2(arr))
+
+
+
