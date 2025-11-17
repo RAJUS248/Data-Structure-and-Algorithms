cfd37421b48a1a9c510f0a4ed9c39c1cd71f360d
Raju S Baradur <125146841+RAJUS248@users.noreply.github.com>
2025-11-17 18:49:05 +0530

    Add files via upload

---
+++ 01_Array or List/47_Next Permutation.py
@@ -0,0 +1,27 @@
+def next_permutation(arr):
+
+    n = len(arr)
+    i = n-2
+
+    while i >= 0 and arr[i] >= arr[i+1]:
+
+        i -= 1
+
+    if i >= 0:
+        j = n -1
+
+        while arr[j] <= arr[i]:
+            j -= 1
+
+        arr[i],arr[j] = arr[j],arr[i]
+
+    left, right = i + 1, n - 1
+    while left < right:
+            arr[left], arr[right] = arr[right], arr[left]
+            left += 1
+            right -= 1
+
+    return arr
+
+arr = [1,3,2,5,4]
+print(next_permutation(arr))
