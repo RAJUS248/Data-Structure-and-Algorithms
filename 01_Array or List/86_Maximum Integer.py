47618ea47ad65354a1018e6dd490ee1ba268f31d
Raju S Baradur <125146841+RAJUS248@users.noreply.github.com>
2026-01-27 19:10:41 +0530

    Add files via upload

---
+++ 01_Array or List/86_Maximum Integer.py
@@ -0,0 +1,27 @@
+def max_int(n):
+
+    if n == 0:
+        return 0
+    
+    if n == 1:
+        return 1
+
+    nums = [0] * (n+1)
+
+    nums[0] = 0
+    nums[1] = 1
+
+    for i in range((n//2) + 1):
+
+        if 2 * i <= n:
+
+            nums[2 * i] = nums[i]
+
+        if 2 * i + 1 <= n:
+
+            nums[2 * i + 1] = nums[i] + nums[i + 1]
+
+    return max(nums)
+
+n = 50000
+print(max_int(n))
