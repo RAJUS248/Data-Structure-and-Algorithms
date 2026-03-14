cf5a583a18713bf29fcdcb6ba7fe5852fc9e28c5
Raju S Baradur <125146841+RAJUS248@users.noreply.github.com>
2026-03-14 19:24:52 +0530

    Update 88_Longest Increasing Subsequence.py

--- 01_Array or List/88_Longest Increasing Subsequence.py
+++ 01_Array or List/88_Longest Increasing Subsequence.py
@@ -4,7 +4,7 @@ def longest_of_LIS(nums):
     LIS = [1] * n
 
     for i in range(1,n):
-        for j in range(i):
+        for j in range(i): 
 
             if nums[i] > nums[j]:
                 LIS[i] = max(LIS[i],LIS[j]+1)
