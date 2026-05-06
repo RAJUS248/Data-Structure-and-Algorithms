b65b50a337ee616d960d2c37ad4a65cd48d2b84b
Raju S Baradur <125146841+RAJUS248@users.noreply.github.com>
2026-05-06 08:34:09 +0530

    Add files via upload

---
+++ 01_Array or List/01_Sliding window/02_ Longest Harmonious Subsequence.py
@@ -0,0 +1,21 @@
+def findLHS(nums):
+
+       
+    seen = {}
+
+    for num in nums:
+        seen[num] = seen.get(num,0)+1
+
+    maxi = 0
+    for i in range(1,len(nums)):
+
+        if i+1 and i in seen:
+            maxi = max(maxi,(seen[i] + seen[i+1]))
+
+    return maxi
+
+nums = [1,3,2,2,5,2,3,7]
+# print(findLHS(nums))
+def
+
+    
