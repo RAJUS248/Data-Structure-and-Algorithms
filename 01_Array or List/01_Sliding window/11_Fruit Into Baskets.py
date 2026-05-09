14c15b609823084355d8b5cbecfced3373c538e8
Raju S Baradur <125146841+RAJUS248@users.noreply.github.com>
2026-05-09 07:24:21 +0530

    Add files via upload

---
+++ 01_Array or List/01_Sliding window/11_Fruit Into Baskets.py
@@ -0,0 +1,29 @@
+def totalFruit(fruits):
+
+    l = 0
+    max_count = 0
+    seen = {}
+
+    for r in range(len(fruits)):
+
+        seen[fruits[r]] = seen.get(fruits[r],0) + 1
+
+        if len(seen) > 2:
+            max_count -= seen[fruits[l]]
+            del seen[fruits[l]]
+            l += 1
+            max_count += 1
+            
+
+        else:
+            max_count += 1
+
+    return max_count
+        
+
+fruits = [1,2,3,2,2]
+# print(totalFruit(fruits))
+
+
+
+
