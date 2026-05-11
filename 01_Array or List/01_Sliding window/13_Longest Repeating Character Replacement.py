c95353ec8e2284369d39420527c4793a21bb5133
Raju S Baradur <125146841+RAJUS248@users.noreply.github.com>
2026-05-11 08:00:07 +0530

    Add files via upload

---
+++ 01_Array or List/01_Sliding window/13_Longest Repeating Character Replacement.py
@@ -0,0 +1,26 @@
+def characterReplacement(s, k):
+
+    l = 0
+    max_len = 0
+    freq = {}
+    max_freq = 0
+    
+    for r in range(len(s)):
+
+        freq[s[r]] = freq.get(s[r],0) + 1
+
+        max_freq = max(max_freq,freq[s[r]])
+
+        if r - l + 1 - max_freq > k: 
+            freq[s[l]] -= 1
+            l += 1
+            
+        max_len = max(max_len,r - l + 1)
+
+
+    return max_len
+
+
+s = "AABBABB"
+k = 1
+print(characterReplacement(s,k))
