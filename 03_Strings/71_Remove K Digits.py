48079eb9061396b3aed4086a163b52c10d589b2e
Raju S Baradur <125146841+RAJUS248@users.noreply.github.com>
2026-05-28 18:45:49 +0530

    Add files via upload

---
+++ 03_Strings/71_Remove K Digits.py
@@ -0,0 +1,23 @@
+def removeKdigits(num, k):
+
+    n = len(num)
+    maxi = num[k-1:n]
+
+    for i in range(n):
+
+        if i <= k:
+            check = num[0:i+1] + num[k+i+1:n]
+
+            if int(check) < int(maxi):
+                maxi = check
+    
+        else:
+            break
+
+    return maxi
+
+num = "5337"
+k = 2
+print(removeKdigits(num,k))
+
+def
