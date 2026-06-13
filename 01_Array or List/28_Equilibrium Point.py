87bba77ad4edab46341fc0de54ffc43693caaece
Raju S Baradur <125146841+RAJUS248@users.noreply.github.com>
2026-06-13 11:53:50 +0530

    Refactor equilibrium point functions for clarity

--- 01_Array or List/28_Equilibrium Point.py
+++ 01_Array or List/28_Equilibrium Point.py
@@ -4,7 +4,7 @@ def findEquilibrium(arr):
         total_sum = sum(arr)
         left_sum = 0
    
-        for i in range(len(arr)):
+        for i in range(len(arr)):  
             right_sum = total_sum - left_sum - arr[i]
 
             if left_sum == right_sum:
