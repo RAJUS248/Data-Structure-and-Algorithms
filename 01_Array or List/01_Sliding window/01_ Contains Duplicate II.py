841e3001a93961c696a634a8c46bad632f3d2cb7
Raju S Baradur <125146841+RAJUS248@users.noreply.github.com>
2026-08-07 23:31:29 +0530

    Refactor containsNearbyDuplicate functions

--- 01_Array or List/01_Sliding window/01_ Contains Duplicate II.py
+++ 01_Array or List/01_Sliding window/01_ Contains Duplicate II.py
@@ -2,7 +2,7 @@ def containsNearbyDuplicate(nums, k):
         
         seen = set() 
         for j in range(len(nums)): 
-
+ 
             if nums[j] not in seen:
                 seen.add(nums[j])
 
