f8f14ce42b28c2b0728bfd592ee1884ed32d457d
Raju S Baradur <125146841+RAJUS248@users.noreply.github.com>
2026-08-11 18:22:18 +0530

    Refactor anagram check functions for clarity

--- 03_Strings/02_Anagram Check.py
+++ 03_Strings/02_Anagram Check.py
@@ -5,7 +5,7 @@ def Anagram_Check(string1,string2):
 print(Anagram_Check("raja","ajar"))
 
   
-def Anagram_Check_v2(string1,string2): 
+def Anagram_Check_v2(string1,string2):  
 
     freq = {}  
     freq1 = {}
