b85cd462616e9798abfd8ab389f172ef74b72851
Raju S Baradur <125146841+RAJUS248@users.noreply.github.com>
2026-09-10 19:20:12 +0530

    Fix indentation in palindrome function

--- 03_Strings/01_Palindrome Check.py
+++ 03_Strings/01_Palindrome Check.py
@@ -2,7 +2,7 @@ def palindrome(string):
 
     start = 0
     end = len(string) - 1
-
+  
     while start < end:  
         if string[start] != string[end]:    
             return False                  
