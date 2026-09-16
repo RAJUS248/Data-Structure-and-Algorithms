aa8bae0d4c2de4fce0af4446023861da9b859ff3
Raju S Baradur <125146841+RAJUS248@users.noreply.github.com>
2026-09-16 07:30:22 +0530

    Fix indentation in TreeNode constructor

--- 08_Trees/02_Tree BFS/02_BFS.py
+++ 08_Trees/02_Tree BFS/02_BFS.py
@@ -1,6 +1,6 @@
 from collections import deque
 class TreeNode:
-    def __init__(self,data):
+    def __init__(self,data):  
         self.data = data
         self.left = None
         self.right = None
