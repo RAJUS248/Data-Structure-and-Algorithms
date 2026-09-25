f507e7ebf6ddecd3ab60e657619587c1ff211b42
Raju S Baradur <125146841+RAJUS248@users.noreply.github.com>
2026-09-25 16:32:29 +0530

    Fix indentation in Trapping Rain Water algorithm

--- 01_Array or List/06_Trapping Rain Water.py
+++ 01_Array or List/06_Trapping Rain Water.py
@@ -5,7 +5,7 @@ cur_max = arr[0]
 for i in range (len(arr)):
     cur_max = max(cur_max,arr[i])
     max_left.append(cur_max)   
-
+  
 max_right = []
 cur_max = arr[-1]   
 for j in range (len(arr)-1,-1,-1):   
